5b098ba28263f7ed41a77adae9be9afa8b2b807f
Viktor Kelemen <viktor@cookpad.jp>
2011-06-03 15:10:48 +0900

    Adding gitignore, render.js for phantomjs, render_cookpad script

---
+++ render.js
@@ -0,0 +1,41 @@
+// render script
+
+var address = "",
+    output = "",
+    w = 0,
+    h = 0;
+
+function renderUrlToFile(url, file, callback) {
+    var page = new WebPage();
+    page.viewportSize = { width: 320, height : 1000 };
+    page.settings.userAgent = "Phantom.js bot";
+
+    page.open(url, function(status){
+       if ( status !== "success") {
+           console.log("Unable to render '" + url + "'");
+       } else {
+           page.render(file);
+       }
+       delete page;
+       callback(url, file);
+    });
+}
+
+
+if (phantom.args.length !== 4) {
+
+  console.log('Usage: rasterize.js URL filename width height');
+  phantom.exit();
+
+} else {
+
+  address = phantom.args[0];
+  file = phantom.args[1];
+  w = phantom.args[2];
+  h = phantom.args[3];
+
+  renderUrlToFile(address, file, function (url, file) {
+    console.log(url + " -> " + file);
+    phantom.exit();
+  });
+}
